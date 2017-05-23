ad497fb1817eb4f2666335e1dbf259081f0cc0c9
Dominic Brown and Spencer Ruiz <dominic@>
2017-05-23 12:05:52 -0700

    add code for clock and alarm features

--- build/js/app.js
+++ build/js/app.js
@@ -1,10 +1,38 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 $(document).ready(function(){
 
+  var time = function() {
+    var myTime = new Date();
+    var hours = myTime.getHours();
+    var minutes = myTime.getMinutes();
+    var seconds = myTime.getSeconds();
+    return (hours + ":" + minutes + ":" + seconds)
+  };
+
+  var userAlarm = null
+
+  $(".clock").text(time());
+
+  setInterval(function() {
+    $(".clock").text(time());
+  }, 1000);
+
   $('#alarm-form').submit(function(event) {
     event.preventDefault();
-    console.log("form submitted!");
+    userAlarm = $('#user-alarm').val();
   });
+
+  var check_alarm = function(time, alarm) {
+    if (time == alarm) {
+      $('#alarm-alert').text('wakeup!');
+    }
+  };
+
+  setInterval(function() {
+    var currentTime = time().slice(0,5)
+    check_alarm(currentTime, userAlarm);
+  }, 1000);
+
 });
 
 },{}]},{},[1]);
--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
 
+  var time = function() {
+    var myTime = new Date();
+    var hours = myTime.getHours();
+    var minutes = myTime.getMinutes();
+    var seconds = myTime.getSeconds();
+    return (hours + ":" + minutes + ":" + seconds)
+  };
+
+  var userAlarm = null
+
+  $(".clock").text(time());
+
+  setInterval(function() {
+    $(".clock").text(time());
+  }, 1000);
+
   $('#alarm-form').submit(function(event) {
     event.preventDefault();
-    console.log("form submitted!");
+    userAlarm = $('#user-alarm').val();
   });
+
+  var check_alarm = function(time, alarm) {
+    if (time == alarm) {
+      $('#alarm-alert').text('wakeup!');
+    }
+  };
+
+  setInterval(function() {
+    var currentTime = time().slice(0,5)
+    check_alarm(currentTime, userAlarm);
+  }, 1000);
+
 });
